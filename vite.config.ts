3dba246044c4e2f5aafc633d68109dff7c44db42
sanoofbrototype <kvspadikkal2@gmail.com>
2026-06-02 17:47:11 +0530

    revert: cross-browser compatibility upgrade

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite';
-import legacy from '@vitejs/plugin-legacy';
 
 export default defineConfig({
   base: './',
-  plugins: [
-    legacy({
-      targets: ['defaults', 'not IE 11']
-    })
-  ],
   server: {
     port: 3000,
     host: '0.0.0.0',
