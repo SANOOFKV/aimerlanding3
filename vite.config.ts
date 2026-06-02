7bf128e360224b9207f862e5cb959c7ea714c926
sanoofbrototype <kvspadikkal2@gmail.com>
2026-06-02 17:44:14 +0530

    chore: full browser compatibility upgrade with PostCSS and Vite Legacy

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite';
+import legacy from '@vitejs/plugin-legacy';
 
 export default defineConfig({
   base: './',
+  plugins: [
+    legacy({
+      targets: ['defaults', 'not IE 11']
+    })
+  ],
   server: {
     port: 3000,
     host: '0.0.0.0',
